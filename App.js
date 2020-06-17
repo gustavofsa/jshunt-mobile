6ac395ec210e99e2a524303014ac86d773a195f4
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-17 20:55:22 -0300

    hello world

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello World!</Text>
     </View>
   );
 }
@@ -12,8 +12,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ce5578',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 24,
+    color: 'whitesmoke',
+  }
 });
