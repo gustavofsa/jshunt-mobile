d9982bceefb6e6de4fe143ba85178128deb2be99
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-18 16:46:07 -0300

    listing products from api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 
-export default function Main() {
+import api from '../services/api';
+
+const Main = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await api.get('/products');
+
+      const { docs } = response.data;
+
+      setProducts(docs);
+    };
+
+    loadProducts();
+
+    console.log(products);
+  }, [])
+
+
+
   return (
     <View>
-      <Text>Página Main</Text>
+      {products.map(product => (
+        <View key={product.id}>
+          <Text>{product.title}</Text>
+          <Text>{product.description}</Text>
+          <Text>{product.url}</Text>
+        </View>
+      ))}
     </View>
   );
 }
+
+export default Main;
