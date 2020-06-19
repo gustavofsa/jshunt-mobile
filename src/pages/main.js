59dbe96e523ae7e14492f4cc8e4f48714755e777
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-18 23:04:50 -0300

    styling list

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
@@ -18,23 +18,80 @@ const Main = () => {
     loadProducts();
   }, [])
 
+  function handleDetail() {
+
+  }
+
   function renderProduct({ item }) {
     return (
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.description}</Text>
+      <View style={styles.productContainer}>
+        <Text style={styles.productTitle}>{item.title}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
+        <TouchableOpacity style={styles.productButton} onPress={handleDetail}>
+          <Text style={styles.productButtonText}>Acessar</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
+        contentContainerStyle={styles.list}
         data={products}
+        keyExtractor={item => item._id}
         renderItem={renderProduct}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa'
+  },
+
+  list: {
+    padding: 20
+  },
+
+  productContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: "#333",
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24
+  },
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#DA552F',
+    fontWeight: 'bold',
+  }
+});
 
 export default Main;
